cbdead877a929addf7248410336facfc8a72ca19
Matthias Leopold <Matthias.Leopold@zuehlke.com>
2017-10-13 20:21:58 +0200

    Dl4J readme

--- MaschineLearning4J/src/main/java/NeuralNetwork/DL4J/MNISTClassifier.java
+++ MaschineLearning4J/src/main/java/NeuralNetwork/DL4J/MNISTClassifier.java
@@ -14,14 +14,11 @@ import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import java.io.IOException;
 
 /**
- *  Class to load a pre-trained Keras Sequential model and test it against the MNIST test data
- *
+ * Class to load a pre-trained Keras Sequential model and test it against the MNIST test data
  * comments:
  * Keras import generaly only works until keras v1.2.2 (might change in dl4j 0.9.1)
- * Reshape layer not supportet in dl4j 0.9.1 but will come with 0.9.2
- *
+ * Reshape and LSTM layer not supportet in dl4j 0.9.1 but will come with 0.9.2
  */
-
 public class MNISTClassifier {
     public static final int outputNum = 10;
     public static int batchSize = 128;
@@ -31,13 +28,20 @@ public class MNISTClassifier {
 
 
     public static void main(String[] args) throws UnsupportedKerasConfigurationException, IOException, InvalidKerasConfigurationException {
-        if(args.length>0 && (args[0].equals("s")|| args[0].contains("sequential"))){
-            launchSequentialDense();
+        if (args.length > 0) {
+            if ((args[0].equals("ffnn"))) {
+                launchSequentialDense();
+            } else if(args[0].equals("cnn")){
+                launchModelCNN();
+            }else{
+                System.out.println("You passed in an invalid parameter");
+                System.out.println("arg: "+args[0]);
+            }
         }else{
-            launchModelCNN();
+            System.out.println("Please pass a program parameter, to define whether to launch the saved CNN or FFNN");
+            System.out.println("Pass \"ffnn\" for the feed forward neural network");
+            System.out.println("Pass \"cnn\" for the convolutional neural network");
         }
-
-
     }
 
     public static void launchSequentialDense() throws UnsupportedKerasConfigurationException, IOException, InvalidKerasConfigurationException {
@@ -49,7 +53,7 @@ public class MNISTClassifier {
 
         DataSetIterator mnistTest = new MnistDataSetIterator(batchSize, false, rngSeed);
 
-        while(mnistTest.hasNext()){
+        while (mnistTest.hasNext()) {
             DataSet next = mnistTest.next();
             INDArray output = network.output(next.getFeatureMatrix()); //get the networks prediction
             evaluator.eval(next.getLabels(), output); //check the prediction against the true class
@@ -69,16 +73,13 @@ public class MNISTClassifier {
         Evaluation evaluator = new Evaluation(outputNum); //create an evaluation object with 10 possible classes
 
         DataSetIterator mnistTest = new MnistDataSetIterator(batchSize, false, rngSeed);
-        while(mnistTest.hasNext()){
+        while (mnistTest.hasNext()) {
             DataSet next = mnistTest.next();
             INDArray batch = next.getFeatureMatrix();
-
-            INDArray reshapedBatch = batch.reshape(batch.shape()[0],1,28,28);//reshape for convolution layer (because dl4j0.9.1 doesnt support Keras.Reshape()
-
+            INDArray reshapedBatch = batch.reshape(batch.shape()[0], 1, 28, 28);//reshape for convolution layer (because dl4j0.9.1 doesnt support Keras.Reshape()
             INDArray output = network.output(reshapedBatch)[0]; //get the networks prediction
             evaluator.eval(next.getLabels(), output); //check the prediction against the true class
         }
-
 
         //success: evaluates the exact same accuracy value as python Keras
         System.out.println(evaluator.stats());
