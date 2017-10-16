7a387235287fe92c119db02a2bde076331691589
Matthias Leopold <Matthias.Leopold@zuehlke.com>
2017-10-16 17:55:28 +0200

    serving

--- Maschine Learning/NeuralNetwork/Tensorflow_Serve_example/serve.py
+++ Maschine Learning/NeuralNetwork/Tensorflow_Serve_example/serve.py
@@ -17,6 +17,18 @@ from tensorflow.examples.tutorials.mnist import input_data
 tf.app.flags.DEFINE_integer('concurrency', 1,
                             'maximum number of concurrent inference requests')
 tf.app.flags.DEFINE_integer('num_tests', 100, 'Number of test images')
-tf.app.flags.DEFINE_string('server', '', 'PredictionService host:port')
+tf.app.flags.DEFINE_string('server', 'localhost:9000', 'PredictionService host:port')
 tf.app.flags.DEFINE_string('work_dir', '../../Data/MNIST_data/', 'Working directory. ')
 FLAGS = tf.app.flags.FLAGS
+
+
+def do_inference(hostport, work_dir, concurrency, num_test):
+    mnist = input_data.read_data_sets(work_dir,one_hot=True)
+    test_data = mnist.test
+    host, port = hostport.split(":")
+    channel = implementations.insecure_channel(host,int(port))
+    stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+
+if __name__=="__main__":
+    print(FLAGS.num_tests)
+    do_inference(FLAGS.server, FLAGS.work_dir, FLAGS.concurrency, FLAGS.num_tests)
