b6299b45b9039e839f50945b921089c07d11fa8a
Matthias Leopold <Matthias.Leopold@zuehlke.com>
2017-10-23 16:05:23 +0200

    serve dok

--- MaschineLearning4J/src/main/java/NeuralNetwork/Tensorflow/InferenceClient.java
+++ MaschineLearning4J/src/main/java/NeuralNetwork/Tensorflow/InferenceClient.java
@@ -4,7 +4,6 @@ import com.google.common.io.Files;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
@@ -29,25 +28,15 @@ public class InferenceClient {
     private static final String Local_URI = "http://localhost:8000/predict";
     private static final String Heroku_URI = "https://stark-savannah-40830.herokuapp.com/predict";
 
+    private static String path = "";
+    private static String Server_URI = Local_URI; //set either to Local_URI or Heroku_URI
+    private static String picFile = "MNIST-5.png";//default
+
     public static void main(String[] args) {
-        String Server_URI = Local_URI;
-        String picFile = "Font-2.png";//default
-        if(args.length>0){
-            if(!args[0].contains(".png"))args[0]=args[0]+".png";
-            File f = new File(picDir+args[0]);
-            if(f.exists() && !f.isDirectory() && Files.getFileExtension(picDir+args[0]).equals("png")) {
-                picFile=args[0];
-                System.out.println("Infering the webservice with given picture at: "+args[0]);
-            }else{
-                System.out.println("The argument that you passed is invalid.");
-                System.out.println("It must be a .png file, which is contained in directory: "+picDir);
-                System.out.println("\nInfering the webservice with default picture: "+picFile);
-            }
-        }else{
-            System.out.println("Infering the webservice with default picture: "+picFile);
-        }
-        String path = picDir + picFile;
+        evaluateArguments(args);
+
         int[][] picture = readPic(path);
+
 
         CloseableHttpClient httpclient = HttpClients.createDefault();
 
@@ -79,11 +68,11 @@ public class InferenceClient {
 
         try {
             //read out the response
-            System.out.println("Response status line: "+response.getStatusLine());
+            System.out.println("Response status line: " + response.getStatusLine());
             HttpEntity entity = response.getEntity();
             InputStream is = entity.getContent();
             String content = IOUtils.toString(is, StandardCharsets.UTF_8);
-            System.out.println("Response content: "+content);
+            System.out.println("Response content: " + content);
             EntityUtils.consume(entity);
         } catch (IOException e) {
             e.printStackTrace();
@@ -106,10 +95,10 @@ public class InferenceClient {
                 .toArray();
 
         BufferedImage image = new BufferedImage(pixels2d[0].length, pixels2d.length, BufferedImage.TYPE_BYTE_GRAY);
-        WritableRaster wr = image.getRaster() ;
-        for (int y=0, count=0 ; y < image.getHeight() ; y++)
-            for (int x=0 ; x < image.getWidth() ; x++, count++)
-                wr.setSample(x, y, 0, pixelsFlat[count]) ;
+        WritableRaster wr = image.getRaster();
+        for (int y = 0, count = 0; y < image.getHeight(); y++)
+            for (int x = 0; x < image.getWidth(); x++, count++)
+                wr.setSample(x, y, 0, pixelsFlat[count]);
 
         try {
             File outputfile = new File("./saved.png");
@@ -121,6 +110,7 @@ public class InferenceClient {
 
     /**
      * Reads the grayscale integers of image at given path
+     *
      * @param path path to image to read pixels
      * @return integer values in array of shape: [height][width]
      */
@@ -144,32 +134,36 @@ public class InferenceClient {
         return imgArrInt;
     }
 
-    private static void exampleGetRequest(CloseableHttpClient httpclient) {
-        HttpGet httpGet = new HttpGet("http://localhost:8000");
-        CloseableHttpResponse response = null;
-        try {
-            response = httpclient.execute(httpGet);
-        } catch (IOException e) {
-            e.printStackTrace();
+    /**
+     * Evaluates the given program parameters to determin the path of the picture used for prediction
+     *
+     * @param args String[] forwarded from the program arguments
+     * @return String of the path to the picture used for prediction
+     */
+    private static void evaluateArguments(String[] args) {
+        if (args.length > 0) {
+            if (!args[0].contains(".png")) args[0] = args[0] + ".png";
+            if (new File(args[0]).isFile() && Files.getFileExtension(args[0]).equals("png")) {
+                path = args[0];
+                System.out.println("Infering the webservice with given picture at: " + path);
+            } else {
+                File f = new File(picDir + args[0]);
+                if (f.isFile() && Files.getFileExtension(picDir + args[0]).equals("png")) {
+                    path = picDir + args[0];
+                    System.out.println("Infering the webservice with given picture at: " + path);
+                } else {
+                    System.out.println("The argument that you passed is invalid.");
+                    System.out.println("It must be a .png file contained in directory: " + picDir + ", or a valid absolute path.");
+                    System.exit(1);
+                }
+            }
+        } else {
+            path = picDir + picFile;
+            System.out.println("Infering the webservice with default picture: " + path);
         }
 
-        try {
-            System.out.println(response.getStatusLine());
-            HttpEntity entity = response.getEntity();
-            InputStream is = entity.getContent();
-            String content = IOUtils.toString(is, StandardCharsets.UTF_8);
-            System.out.println(content);
-            is.close();
-
-            EntityUtils.consume(entity);//frees the ressource
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                response.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        if (Server_URI.equals(Heroku_URI)) {
+            System.out.println("Infering the webservice hosted on Heroku. As I am using the free version, Heroku might be unusually slow to respond.");
         }
     }
 }
